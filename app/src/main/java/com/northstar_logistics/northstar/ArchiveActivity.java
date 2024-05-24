b0e28b7feb6a0c0dacbb9032af8f7ecf2bd9c3ff
pressf1om <wwwsasha.www@yandex.ru>
2024-05-24 23:07:37 +0300

    Добавлено отображение архивных заявок

--- app/src/main/java/com/northstar_logistics/northstar/ArchiveActivity.java
+++ app/src/main/java/com/northstar_logistics/northstar/ArchiveActivity.java
@@ -13,10 +13,15 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class ArchiveActivity extends AppCompatActivity {
 
-    TextView car_number_text;
+    TextView car_number_text, main_text;
     String number_car;
 
     @Override
@@ -26,6 +31,7 @@ public class ArchiveActivity extends AppCompatActivity {
 
         // Ищем элемент по ID
         car_number_text = findViewById(R.id.upperTxt);
+        main_text = findViewById(R.id.main_text);
 
         // Получаем путь к файлу car_number.txt внутреннего хранилища приложения
         File file = new File(getFilesDir(), "car_number.txt");
@@ -50,6 +56,56 @@ public class ArchiveActivity extends AppCompatActivity {
             e.printStackTrace();
             Toast.makeText(getApplicationContext(), "Ошибка при чтении данных из файла", Toast.LENGTH_SHORT).show();
         }
+
+        // Вызываем метод для выполнения GET-запроса
+        getData(number_car);
+    }
+
+    // Метод для выполнения GET-запроса к серверу
+    private void getData(String carNumber) {
+        OkHttpClient client = new OkHttpClient();
+
+        // Создаем запрос к указанному URL с номером машины
+        Request request = new Request.Builder()
+                .url("http://northstar-logistics.ru/api/completed_applications/" + carNumber)
+                .build();
+
+        // Отправляем запрос асинхронно
+        client.newCall(request).enqueue(new Callback() {
+            // в случае ошибки
+            @Override
+            public void onFailure(Call call, IOException e) {
+                // В случае ошибки выводим сообщение об ошибке
+                e.printStackTrace();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(ArchiveActivity.this, "Ошибка при выполнении запроса", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            // успешный запрос
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    // Получаем тело ответа и выводим его в логи
+                    final String responseData = response.body().string();
+                    Log.d("ArchiveActivity", "Ответ на GET-запрос: " + responseData);
+
+                    // Обновляем TextView с полученными данными
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            main_text.setText(responseData);
+                        }
+                    });
+                } else {
+                    Log.e("ArchiveActivity", "Ошибка при выполнении GET-запроса: " + response.code());
+                }
+            }
+
+        });
     }
 
     // Боковое меню
